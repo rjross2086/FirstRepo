6dc66fb9c2d4128810647573b922e97bfc2b9e6f
rjross2086 <42652462+rjross2086@users.noreply.github.com>
2019-10-03 16:36:18 -0400

    Add files via upload

---
+++ nsswitch_bash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo cat << 'EOF' > file.txt
+# /etc/nsswitch.conf
+#
+# Example configuration of GNU Name Service Switch functionality.
+# If you have the `glibc-doc-reference' and `info' packages installed, try:
+# `info libc "Name Service Switch"' for information about this file.
+
+passwd:         compat
+group:          compat
+replacement-line
+gshadow:        files
+
+hosts:          files cache dns
+networks:       files
+
+protocols:      db files
+services:       db files
+ethers:         db files
+rpc:            db files
+
+netgroup:       nis
+EOF
+
+sudo cp /etc/nsswitch.conf /etc/old_nsswitch.conf
+sudo mv file.txt /etc/nsswitch.conf
+
+echo "End of Script"
